2212a8a50da9ce4dec1429738dd2ab6deb5aa0b0
k121mayur <59823597+k121mayur@users.noreply.github.com>
2020-07-11 17:02:07 +0530

    Update and rename index.html to index.php
    just testing...

---
+++ index.php
@@ -0,0 +1,5 @@
+<h1>Water Knowledge Centre Project</h1>
+<p>Water Knowledge Centre is the department on The DHAN Academy, Madurai which works on Reasearch and Water related development work.
+present software has been made for entering data of the test conducted by Water Knowledge Centre</p>
+<?php
+echo "test is successful" ?>
